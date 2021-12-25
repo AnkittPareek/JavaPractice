b01842e9dab6a679290f5e1f44b380d2e4f6f427
Ankit <akpareek4@gmail.com>
2021-12-25 21:19:48 +0530

    More Practice

---
+++ src/com/company/CreateDoublyCircularLL.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+
+class CreateDoublyCircularLL {
+    Node head;
+    // Creating structure for nodes
+    class Node {
+        //write your code here
+    }
+
+    // This method will push the input data at the end of the list
+    void push(int data) {
+        //write your code here
+    }
+
+    void print() {
+        //write your code here
+    }
+    public static void main(String[] args) {
+        CreateDoublyCircularLL obj = new CreateDoublyCircularLL();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            obj.push(in.nextInt());
+        }
+        obj.print();
+    }
+}
